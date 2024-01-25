32b73c4943b4d3c9a897d438b17dc5d6c78d74a3
wangsijie <sijiewg@gmail.com>
2024-01-25 10:25:22 +0800

    refactor: use workers

---
+++ src/index.ts
@@ -0,0 +1,204 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+import { CryptoStats, GlobalVolume, stringifyCryptoStats, stringifyGlobalVolume } from './utils';
+
+export interface Env {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+	//
+	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+	// MY_SERVICE: Fetcher;
+	//
+	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
+	// MY_QUEUE: Queue;
+
+	BOT_TOKEN: string;
+	CHAT_ID: string;
+	CMC_API_KEY: string;
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		return new Response('Hello World!');
+	},
+	async scheduled(event: Event, env: Env, ctx: ExecutionContext) {
+		const botToken = env.BOT_TOKEN;
+		const baseUrl = `https://api.telegram.org/bot${botToken}`;
+		const chatId = env.CHAT_ID;
+		const cmcApiKey = env.CMC_API_KEY;
+
+		const sendMessage = async (message: string) => {
+			if (!chatId) {
+				console.log('No chat id, skip sending message', message);
+				return;
+			}
+
+			const url = `${baseUrl}/sendMessage`;
+			const response = await fetch(url, {
+				body: JSON.stringify({
+					chat_id: Number(chatId),
+					text: message,
+				}),
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+				},
+			});
+
+			if (!response.ok) {
+				throw new Error('Failed to send message');
+			}
+		};
+
+		const getGlobalMetrics = async (): Promise<{
+			btc: number;
+			eth: number;
+			globalVolume: GlobalVolume;
+		}> => {
+			type ApiResponseType = {
+				data: {
+					btc_dominance: number;
+					eth_dominance: number;
+					quote: {
+						USD: GlobalVolume;
+					};
+				};
+			};
+
+			if (!cmcApiKey) {
+				throw new Error('No CMC API key');
+			}
+
+			const response = await fetch(
+				'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest',
+				{
+					headers: {
+						'X-CMC_PRO_API_KEY': cmcApiKey,
+					},
+				}
+			);
+
+			if (!response.ok) {
+				throw new Error('Failed to fetch coinmarketcap.com');
+			}
+
+			const json = (await response.json()) as ApiResponseType;
+
+			const {
+				btc_dominance,
+				eth_dominance,
+				quote: {
+					USD: {
+						total_volume_24h,
+						total_market_cap,
+						total_market_cap_yesterday,
+						total_volume_24h_yesterday,
+						total_market_cap_yesterday_percentage_change,
+						total_volume_24h_yesterday_percentage_change,
+					},
+				},
+			} = json.data;
+
+			return {
+				btc: btc_dominance,
+				eth: eth_dominance,
+				globalVolume: {
+					total_volume_24h,
+					total_market_cap,
+					total_market_cap_yesterday,
+					total_volume_24h_yesterday,
+					total_market_cap_yesterday_percentage_change,
+					total_volume_24h_yesterday_percentage_change,
+				},
+			};
+		};
+
+		const getLatestStats = async (): Promise<{ btc?: CryptoStats; eth?: CryptoStats }> => {
+			type DataPayload = {
+				id: number;
+				quote: {
+					USD: CryptoStats;
+				};
+			};
+
+			type ApiResponseType = {
+				data: DataPayload[];
+			};
+
+			if (!cmcApiKey) {
+				throw new Error('No CMC API key');
+			}
+
+			const response = await fetch(
+				'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+				{
+					headers: {
+						'X-CMC_PRO_API_KEY': cmcApiKey,
+					},
+				}
+			);
+
+			if (!response.ok) {
+				throw new Error('Failed to fetch coinmarketcap.com');
+			}
+
+			const json = (await response.json()) as ApiResponseType;
+
+			if (!json || !json.data) {
+				throw new Error('Failed to latest stats');
+			}
+
+			const btcPayload = json.data.find(({ id }) => id === 1); // BTC coinMarketCap id 1
+			const ethPayload = json.data.find(({ id }) => id === 1027); // ETH coinMarketCap id 1027
+
+			return { btc: btcPayload?.quote.USD, eth: ethPayload?.quote.USD };
+		};
+
+		const getFearIndex = async (): Promise<[number, number]> => {
+			const response = await fetch('https://api.coin-stats.com/v2/fear-greed');
+
+			if (!response.ok) {
+				throw new Error('Failed to fetch api.coin-stats.com');
+			}
+
+			const json = (await response.json()) as {
+				now: { value: string };
+				yesterday: { value: string };
+			};
+
+			return [Number(json.now.value), Number(json.yesterday.value)];
+		};
+
+		const [score, yesterdayScore] = await getFearIndex();
+		const { btc, globalVolume } = await getGlobalMetrics();
+		const { btc: btcStats } = await getLatestStats();
+
+		await sendMessage(
+			[
+				`贪婪指数: ${Math.floor(score)}（昨日: ${Math.floor(yesterdayScore)}）`,
+				'',
+				...stringifyGlobalVolume(globalVolume),
+				'',
+				`BTC 占比: ${btc.toFixed(2)}%`,
+				...(btcStats
+					? stringifyCryptoStats({ ...btcStats, total_volume_24h: globalVolume.total_volume_24h })
+					: []),
+				'',
+			].join('\n')
+		);
+	},
+};
---
+++ src/utils.ts
@@ -0,0 +1,63 @@
+export type CryptoStats = {
+	price: number;
+	volume_24h: number;
+	percent_change_1h: number;
+	percent_change_24h: number;
+	market_cap: number;
+};
+
+export type GlobalVolume = {
+	total_volume_24h: number;
+	total_market_cap: number;
+	total_market_cap_yesterday: number;
+	total_volume_24h_yesterday: number;
+	total_market_cap_yesterday_percentage_change: number;
+	total_volume_24h_yesterday_percentage_change: number;
+};
+
+export const formatNumberWithCommas = (number_: number): string =>
+	number_.toFixed(2).replaceAll(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+export const prettifyBigNumber = (number_: number): string => {
+	const million = 1_000_000;
+	const billion = 1_000_000_000;
+	const trillion = 1_000_000_000_000;
+	if (number_ < million) {
+		return formatNumberWithCommas(number_);
+	}
+	if (million <= number_ && number_ < billion) {
+		return `${formatNumberWithCommas(number_ / million)} m`;
+	}
+	if (billion <= number_ && number_ < trillion) {
+		return `${formatNumberWithCommas(number_ / billion)} b`;
+	}
+	return `${formatNumberWithCommas(number_ / trillion)} t`;
+};
+
+export const stringifyCryptoStats = (
+	stats: CryptoStats & { total_volume_24h?: number }
+): string[] => {
+	const { price, volume_24h, percent_change_1h, percent_change_24h, market_cap, total_volume_24h } =
+		stats;
+	return [
+		`价格: ${price.toFixed(2)} USD`,
+		`1 小时涨跌幅: ${percent_change_1h.toFixed(2)}%`,
+		`24 小时涨跌幅: ${percent_change_24h.toFixed(2)}%`,
+		`24 小时换手率: ${((100 * volume_24h) / market_cap).toFixed(2)}%`,
+		`24 小时成交量: ${prettifyBigNumber(volume_24h)}`,
+		...(total_volume_24h
+			? [`24 小时成交额占比: ${((100 * volume_24h) / total_volume_24h).toFixed(2)}%`]
+			: []),
+	];
+};
+
+export const stringifyGlobalVolume = (stats: GlobalVolume): string[] => {
+	const {
+		total_market_cap_yesterday_percentage_change,
+		total_volume_24h_yesterday_percentage_change,
+	} = stats;
+	return [
+		`市值变化: ${total_market_cap_yesterday_percentage_change.toFixed(2)}%`,
+		`成交额变化: ${total_volume_24h_yesterday_percentage_change.toFixed(2)}%`,
+	];
+};
