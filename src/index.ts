aceb6e6675ed31c6f05e8fb0742c2cb68bbc7313
wangsijie <sijiewg@gmail.com>
2024-02-07 17:15:05 +0800

    feat: add ordi

--- src/index.ts
+++ src/index.ts
@@ -31,92 +31,75 @@ export interface Env {
 	CMC_API_KEY: string;
 }
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-	async scheduled(event: Event, env: Env, ctx: ExecutionContext) {
-		const botToken = env.BOT_TOKEN;
-		const baseUrl = `https://api.telegram.org/bot${botToken}`;
-		const chatId = env.CHAT_ID;
-		const cmcApiKey = env.CMC_API_KEY;
+const sendMessage = async (message: string, env: Env) => {
+	const botToken = env.BOT_TOKEN;
+	const baseUrl = `https://api.telegram.org/bot${botToken}`;
+	const chatId = env.CHAT_ID;
 
-		const sendMessage = async (message: string) => {
-			if (!chatId) {
-				console.log('No chat id, skip sending message', message);
-				return;
-			}
+	if (!chatId) {
+		console.log('No chat id, skip sending message', message);
+		return;
+	}
 
-			const url = `${baseUrl}/sendMessage`;
-			const response = await fetch(url, {
-				body: JSON.stringify({
-					chat_id: Number(chatId),
-					text: message,
-				}),
-				method: 'POST',
-				headers: {
-					'content-type': 'application/json;charset=UTF-8',
-				},
-			});
+	const url = `${baseUrl}/sendMessage`;
+	const response = await fetch(url, {
+		body: JSON.stringify({
+			chat_id: Number(chatId),
+			text: message,
+		}),
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json;charset=UTF-8',
+		},
+	});
 
-			if (!response.ok) {
-				throw new Error('Failed to send message');
-			}
-		};
+	if (!response.ok) {
+		throw new Error('Failed to send message');
+	}
+};
 
-		const getGlobalMetrics = async (): Promise<{
-			btc: number;
-			eth: number;
-			globalVolume: GlobalVolume;
-		}> => {
-			type ApiResponseType = {
-				data: {
-					btc_dominance: number;
-					eth_dominance: number;
-					quote: {
-						USD: GlobalVolume;
-					};
+const handler = async (env: Env) => {
+	const cmcApiKey = env.CMC_API_KEY;
+
+	const getGlobalMetrics = async (): Promise<{
+		btc: number;
+		eth: number;
+		globalVolume: GlobalVolume;
+	}> => {
+		type ApiResponseType = {
+			data: {
+				btc_dominance: number;
+				eth_dominance: number;
+				quote: {
+					USD: GlobalVolume;
 				};
 			};
+		};
 
-			if (!cmcApiKey) {
-				throw new Error('No CMC API key');
-			}
+		if (!cmcApiKey) {
+			throw new Error('No CMC API key');
+		}
 
-			const response = await fetch(
-				'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest',
-				{
-					headers: {
-						'X-CMC_PRO_API_KEY': cmcApiKey,
-					},
-				}
-			);
-
-			if (!response.ok) {
-				throw new Error('Failed to fetch coinmarketcap.com');
-			}
-
-			const json = (await response.json()) as ApiResponseType;
-
-			const {
-				btc_dominance,
-				eth_dominance,
-				quote: {
-					USD: {
-						total_volume_24h,
-						total_market_cap,
-						total_market_cap_yesterday,
-						total_volume_24h_yesterday,
-						total_market_cap_yesterday_percentage_change,
-						total_volume_24h_yesterday_percentage_change,
-					},
+		const response = await fetch(
+			'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest',
+			{
+				headers: {
+					'X-CMC_PRO_API_KEY': cmcApiKey,
 				},
-			} = json.data;
+			}
+		);
 
-			return {
-				btc: btc_dominance,
-				eth: eth_dominance,
-				globalVolume: {
+		if (!response.ok) {
+			throw new Error('Failed to fetch coinmarketcap.com');
+		}
+
+		const json = (await response.json()) as ApiResponseType;
+
+		const {
+			btc_dominance,
+			eth_dominance,
+			quote: {
+				USD: {
 					total_volume_24h,
 					total_market_cap,
 					total_market_cap_yesterday,
@@ -124,111 +107,141 @@ export default {
 					total_market_cap_yesterday_percentage_change,
 					total_volume_24h_yesterday_percentage_change,
 				},
+			},
+		} = json.data;
+
+		return {
+			btc: btc_dominance,
+			eth: eth_dominance,
+			globalVolume: {
+				total_volume_24h,
+				total_market_cap,
+				total_market_cap_yesterday,
+				total_volume_24h_yesterday,
+				total_market_cap_yesterday_percentage_change,
+				total_volume_24h_yesterday_percentage_change,
+			},
+		};
+	};
+
+	const getLatestStats = async (): Promise<{
+		btc?: CryptoStats;
+		eth?: CryptoStats;
+		ordi?: CryptoStats;
+	}> => {
+		type DataPayload = {
+			id: number;
+			symbol: string;
+			quote: {
+				USD: CryptoStats;
 			};
 		};
 
-		const getLatestStats = async (): Promise<{ btc?: CryptoStats; eth?: CryptoStats }> => {
-			type DataPayload = {
-				id: number;
-				quote: {
-					USD: CryptoStats;
-				};
-			};
-
-			type ApiResponseType = {
-				data: DataPayload[];
-			};
-
-			if (!cmcApiKey) {
-				throw new Error('No CMC API key');
-			}
-
-			const response = await fetch(
-				'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
-				{
-					headers: {
-						'X-CMC_PRO_API_KEY': cmcApiKey,
-					},
-				}
-			);
-
-			if (!response.ok) {
-				throw new Error('Failed to fetch coinmarketcap.com');
-			}
-
-			const json = (await response.json()) as ApiResponseType;
-
-			if (!json || !json.data) {
-				throw new Error('Failed to latest stats');
-			}
-
-			const btcPayload = json.data.find(({ id }) => id === 1); // BTC coinMarketCap id 1
-			const ethPayload = json.data.find(({ id }) => id === 1027); // ETH coinMarketCap id 1027
-
-			return { btc: btcPayload?.quote.USD, eth: ethPayload?.quote.USD };
+		type ApiResponseType = {
+			data: DataPayload[];
 		};
 
-		const getFearIndex = async (): Promise<[number, number]> => {
-			const response = await fetch('https://api.coin-stats.com/v2/fear-greed');
+		if (!cmcApiKey) {
+			throw new Error('No CMC API key');
+		}
 
-			if (!response.ok) {
-				throw new Error('Failed to fetch api.coin-stats.com');
+		const response = await fetch(
+			'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+			{
+				headers: {
+					'X-CMC_PRO_API_KEY': cmcApiKey,
+				},
 			}
-
-			const json = (await response.json()) as {
-				now: { value: string };
-				yesterday: { value: string };
-			};
-
-			return [Number(json.now.value), Number(json.yesterday.value)];
-		};
-
-		const getFundingRate = async (): Promise<number> => {
-			const response = await fetch(
-				'https://www.okx.com/api/v5/public/funding-rate?instId=BTC-USDT-SWAP'
-			);
-
-			if (!response.ok) {
-				throw new Error('Failed to fetch okx');
-			}
-
-			const json = (await response.json()) as {
-				data: Array<{
-					fundingRate: string;
-				}>;
-			};
-
-			const rate = json.data[0]?.fundingRate;
-
-			if (!rate) {
-				throw new Error('Failed to fetch funding rate');
-			}
-
-			return Math.round(Number(rate) * 100 * 1000) / 1000;
-		};
-
-		const [score, yesterdayScore] = await getFearIndex();
-		const { btc, globalVolume } = await getGlobalMetrics();
-		const { btc: btcStats } = await getLatestStats();
-		const fundingRate = await getFundingRate();
-
-		await sendMessage(
-			[
-				`贪婪指数: ${Math.floor(score)}（昨日: ${Math.floor(yesterdayScore)}）`,
-				`BTC: ${btcStats && Math.floor(btcStats.price)}`,
-				`合约费率: ${fundingRate}%`,
-				'',
-				...(btcStats
-					? stringifyCryptoStats({
-							...btcStats,
-							total_volume_24h: globalVolume.total_volume_24h,
-							dominance: btc,
-					  })
-					: []),
-				'',
-				...stringifyGlobalVolume(globalVolume),
-				'',
-			].join('\n')
 		);
+
+		if (!response.ok) {
+			throw new Error('Failed to fetch coinmarketcap.com');
+		}
+
+		const json = (await response.json()) as ApiResponseType;
+
+		if (!json || !json.data) {
+			throw new Error('Failed to latest stats');
+		}
+
+		const btcPayload = json.data.find(({ symbol }) => symbol === 'BTC'); // BTC coinMarketCap id 1
+		const ethPayload = json.data.find(({ symbol }) => symbol === 'ETH'); // ETH coinMarketCap id 1027
+		const ordiPayload = json.data.find(({ symbol }) => symbol === 'ORDI');
+
+		return { btc: btcPayload?.quote.USD, eth: ethPayload?.quote.USD, ordi: ordiPayload?.quote.USD };
+	};
+
+	const getFearIndex = async (): Promise<[number, number]> => {
+		const response = await fetch('https://api.coin-stats.com/v2/fear-greed');
+
+		if (!response.ok) {
+			throw new Error('Failed to fetch api.coin-stats.com');
+		}
+
+		const json = (await response.json()) as {
+			now: { value: string };
+			yesterday: { value: string };
+		};
+
+		return [Number(json.now.value), Number(json.yesterday.value)];
+	};
+
+	const getFundingRate = async (): Promise<number> => {
+		const response = await fetch(
+			'https://www.okx.com/api/v5/public/funding-rate?instId=BTC-USDT-SWAP'
+		);
+
+		if (!response.ok) {
+			throw new Error('Failed to fetch okx');
+		}
+
+		const json = (await response.json()) as {
+			data: Array<{
+				fundingRate: string;
+			}>;
+		};
+
+		const rate = json.data[0]?.fundingRate;
+
+		if (!rate) {
+			throw new Error('Failed to fetch funding rate');
+		}
+
+		return Math.round(Number(rate) * 100 * 1000) / 1000;
+	};
+
+	const [score, yesterdayScore] = await getFearIndex();
+	const { btc, globalVolume } = await getGlobalMetrics();
+	const { btc: btcStats, eth: ethStats, ordi: ordiStats } = await getLatestStats();
+	const fundingRate = await getFundingRate();
+
+	return [
+		`贪婪指数: ${Math.floor(score)}（昨日: ${Math.floor(yesterdayScore)}）`,
+		`BTC: ${btcStats && Math.floor(btcStats.price)}`,
+		`合约费率: ${fundingRate}%`,
+		`ETH: ${ethStats && Math.floor(ethStats.price)}`,
+		`ORDI: ${ordiStats && ordiStats.price.toPrecision(4)}`,
+		'',
+		...(btcStats
+			? stringifyCryptoStats({
+					...btcStats,
+					total_volume_24h: globalVolume.total_volume_24h,
+					dominance: btc,
+			  })
+			: []),
+		'',
+		...stringifyGlobalVolume(globalVolume),
+		'',
+	].join('\n');
+};
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const message = await handler(env);
+		return new Response(message);
+	},
+	async scheduled(event: Event, env: Env, ctx: ExecutionContext) {
+		const message = await handler(env);
+		await sendMessage(message, env);
 	},
 };
